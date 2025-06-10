613879167e0b17d0e2f22c5ad9aa3211fb4961e6
Paka360 <spakalnahkey@gmail.com>
2025-06-10 20:59:22 +0000

    Create working_with_lists.py
    For this lesson, I'm learning how to use for loops and work with my list items using few lines of code.

---
+++ working_with_lists.py
@@ -0,0 +1,13 @@
+#Working with lists
+
+#looping through an entire lists
+animals = ['lion', 'cat', 'dog', 'zebra', 'snake', 'goat']
+for animal in animals:
+    print(animal)
+
+countries = ['ghana', 'benin', 'usa', 'uk', 'china']
+for country in countries:
+    print(country.title() + " is the best country to be born in.")
+    print('I love the people and everything else about ' + country.title()+ ".\n")
+
+print("Every country is unique and special")
