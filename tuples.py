785937537058b64736c3801894c15e58f3fb11c9
Paka360 <spakalnahkey@gmail.com>
2025-06-13 19:37:44 +0000

    Tuples ( Permanent lists)
    I learnt how tuples work and their importance. I also read about the PEP 8 style guidelines.

---
+++ tuples.py
@@ -0,0 +1,29 @@
+#Tuples
+dimensions = (200,50)
+print(dimensions[0])
+print(dimensions[1])
+
+for dimension in dimensions:
+    print(dimension)
+
+primes = (3,5,7,11,13,17,19)
+print("Original Primes")
+for prime in primes:
+    print(prime)
+
+primes = (23,29,31,37,41)
+print("Modified Primes")
+for prime in primes:
+    print(prime)
+
+#Practice Exercise
+print('\nMenu')
+buffets = ('tea', 'coffee', 'porrige', 'milk_shake', 'soakings')
+for buffet in buffets:
+    print(buffet.title())
+
+buffets = ('waakye', 'fufu', 'kokonte', 'tuozaafi', 'rice')
+print('\nRevised Menu')
+for buffet in buffets:
+    print(buffet.title())
+
