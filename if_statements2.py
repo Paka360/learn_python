3f50efb93ae65d20fd5720307206070bbfa9fb3b
Paka360 <spakalnahkey@gmail.com>
2025-06-21 20:50:56 +0000

    Update if_statements2.py

--- if_statements2.py
+++ if_statements2.py
@@ -74,4 +74,14 @@ for new_user in new_users:
         print (new_user +". This username is not available. Please try a new username.\n")
     else:
         print (new_user + ". This username is available.\n")
-    
+
+positions = [1,2,3,4,5,6,7,8,9]
+for position in positions:
+    if position == 1:
+        print(str(position) + 'st')
+    elif position == 2:
+        print(str(position) + 'nd')
+    elif position == 3:
+        print(str(position) + 'rd')
+    else:
+        print(str(position) + 'th')
