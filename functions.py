d2b1d080dddab3da3ab4cbfa08ded93d14edcfef
Paka360 <spakalnahkey@gmail.com>
2025-07-21 12:56:21 +0000

    Update functions.py

--- functions.py
+++ functions.py
@@ -95,74 +95,113 @@ musician = get_formatted_name('king', 'promise')
 print(musician)
 
 #Making an argument optional
-def get_formatted_name(firstname, lastname,  middlename = ''):
-    """Return a fullname, neatly formatted"""
-    if middlename:
-        fullname =  fullname = firstname+' ' +middlename+ ' ' +lastname
-    else:
-        fullname = firstname+ ' ' +lastname
-    return fullname.title()
-musician = get_formatted_name('king', 'promise')
-print(musician)
-artist = get_formatted_name('celine', 'dion', 'anna')
-print(artist)
-
-#Returning a dictionary
-def build_person(firstname, lastname, age =""):
-    """Return a dictionary of information about a person"""
-    person = {'first': firstname, 'last':lastname}
-    return person
-
-actor = build_person('tom', 'cruise')
-print(actor)
-
-def build_person(firstname, lastname, age =""):
-    """Return a dictionary of information about a person"""
-    person = {'first': firstname, 'last':lastname}
-    if age:
-        person['age'] = age
-    return person
-
-actor = build_person('tom', 'cruise', age = 52)
-print(actor)
-
-#Using a function with a while loop
-# def get_formatted_name(firstname, lastname):
-#     """Return a full name neatly formatted"""
-#     fullname = firstname + ' '+ lastname
+# def get_formatted_name(firstname, lastname,  middlename = ''):
+#     """Return a fullname, neatly formatted"""
+#     if middlename:
+#         fullname =  fullname = firstname+' ' +middlename+ ' ' +lastname
+#     else:
+#         fullname = firstname+ ' ' +lastname
 #     return fullname.title()
+# musician = get_formatted_name('king', 'promise')
+# print(musician)
+# artist = get_formatted_name('celine', 'dion', 'anna')
+# print(artist)
 
+# #Returning a dictionary
+# def build_person(firstname, lastname, age =""):
+#     """Return a dictionary of information about a person"""
+#     person = {'first': firstname, 'last':lastname}
+#     return person
+
+# actor = build_person('tom', 'cruise')
+# print(actor)
+
+# def build_person(firstname, lastname, age =""):
+#     """Return a dictionary of information about a person"""
+#     person = {'first': firstname, 'last':lastname}
+#     if age:
+#         person['age'] = age
+#     return person
+
+# actor = build_person('tom', 'cruise', age = 52)
+# print(actor)
+
+# #Using a function with a while loop
+# # def get_formatted_name(firstname, lastname):
+# #     """Return a full name neatly formatted"""
+# #     fullname = firstname + ' '+ lastname
+# #     return fullname.title()
+
+# # while True:
+# #     print("\nPlease enter your name: ")
+# #     print("(Enter quit when you are done)")
+
+# #     f_name = input("First name: ")
+# #     if f_name =="quit":
+# #         break
+
+# #     l_name = input("\nLast name: ")
+# #     if l_name == "quit":
+# #         break
+
+# #     formatted_name = get_formatted_name(f_name, l_name)
+# #     print("\nHello, " + formatted_name +"!") 
+
+# #Practice Exercise
+# def capital_towns(country, capital):
+#     country = country + ", " + capital
+#     return country.title()
+
+# ghana = capital_towns('ghana', 'accra')
+# print(ghana)
+# china = capital_towns('china', 'hong kong')
+# print(china)
+# russia = capital_towns('russia', 'moscow')
+# print(russia)
+
+# def build_album(artist, title):
+#     """Describing an album"""
+#     album = {'art':artist, 'tit':title}
+#     return album
+
+# album_1 = build_album('ed sheeran', 'divide') 
+# print(album_1)
+
+# def build_album(artist, title, tracks=""):
+#     """Describing an album"""
+#     album = {'art':artist, 'tit':title}
+#     if tracks:
+#         album['tracks'] = tracks
+#     return album
+
+# album_1 = build_album('ed sheeran', 'divide', '12') 
+# print(album_1)
+
+# def build_album(artist, title, tracks=""):
+#     """Describing an album"""
+#     album = {'art':artist, 'tit':title}
+#     return album
 # while True:
-#     print("\nPlease enter your name: ")
-#     print("(Enter quit when you are done)")
-
-#     f_name = input("First name: ")
-#     if f_name =="quit":
+#     print("\nEnter the album information: ")
+#     print("(Please enter 'quit' when you are done)")
+    
+#     musician = input("What is the name of the artist: ")
+#     if musician == 'quit':
 #         break
 
-#     l_name = input("\nLast name: ")
-#     if l_name == "quit":
+#     title1 = input("What is the title of the album: ")
+#     if title1 == 'quit':
 #         break
 
-#     formatted_name = get_formatted_name(f_name, l_name)
-#     print("\nHello, " + formatted_name +"!") 
+#     album_1 = build_album(musician, title1,) 
+#     print(album_1)
 
-#Practice Exercise
-def capital_towns(country, capital):
-    country = country + ", " + capital
-    return country.title()
+#Passing a list
+def greet_users(names):
+    """Print a simple message to each user in the lost"""
+    for name in names:
+        message = "Hello, "+ name.title() + '!'
+        print(message)
 
-ghana = capital_towns('ghana', 'accra')
-print(ghana)
-china = capital_towns('china', 'hong kong')
-print(china)
-russia = capital_towns('russia', 'moscow')
-print(russia)
-
-def build_album(artist, title):
-    """Describing an album"""
-    album = {'art':artist, 'tit':title}
-    return album
-
-album_1 = build_album('ed sheeran', 'divide') 
-print(album_1)
+usernames = ['hannah', 'tyland', 'john']
+greet_users(usernames)
