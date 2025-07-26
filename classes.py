4f34d34d754eff73c90d66ec166ff48d871f2030
Paka360 <spakalnahkey@gmail.com>
2025-07-26 20:02:21 +0000

    Create classes.py
    I learnt about how we use classes to model real world entitiies and how to create them

---
+++ classes.py
@@ -0,0 +1,66 @@
+#Classes
+#Creating and using a class
+#Creating the Dog Class
+class Dog():
+    """A simple attempt to model a dog"""
+
+    def __init__(self, name, age):
+        """Initialize name and age attributes"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command"""
+        print(self.name.title() + " is now sitting.")
+
+    def roll_over(self):
+        """Simulate a dog rolling over."""
+        print(self.name.title() + " rolled over!")
+
+#Making an instance From a Class
+my_dog = Dog('max',6)
+
+print("My dog's name is "+ my_dog.name.title() +'.')
+print("My dog is "+str(my_dog.age)+ ' years old.')
+
+my_dog.sit()
+my_dog.roll_over()
+
+#Creating Multiple Instances
+my_dog = Dog('max', 10)
+your_dog = Dog('lucy', 7)
+
+print("My dog's name is "+ my_dog.name.title() +'.')
+print("My dog is "+str(my_dog.age)+ ' years old.')
+my_dog.sit()
+
+print("Your dog's name is "+ your_dog.name.title() +'.')
+print("Your dog is "+str(your_dog.age)+ ' years old.')
+your_dog.sit()
+
+#Practice Exercise
+class Restaurant():
+    """Making a model of a restaurant"""
+
+    def __init__(self, name, location):
+        """Initializing name and location attributes"""
+        self.name = name
+        self.location = location
+    
+    def open(self):
+        """Opens the restaurant"""
+        print("You are warmly Welcome")
+
+    def closed(self):
+        """Closes the restaurant"""
+        print("We are closed. Sorry for any inconvenience caused.")
+
+fav_restaurant = Restaurant('Calabash', 'Engineering gate')
+print("Mike lets go and buy food at " + fav_restaurant.name.title() + ".")
+print("The distance is not far. It is located at " + fav_restaurant.location.title())
+fav_restaurant.open()
+fav_restaurant.closed()
+
+food_joint = Restaurant('silver lobster', 'republic hall')
+bush_canteen = Restaurant('chickenman', 'commercial area')
+
