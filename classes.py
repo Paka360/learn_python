916f2ba4f44acaa2fca326f24917ad75b7ef0342
Paka360 <spakalnahkey@gmail.com>
2025-08-02 21:31:23 +0000

    Update classes.py

--- classes.py
+++ classes.py
@@ -217,6 +217,116 @@ fav_restaurant.close_restaurant()
 fav_restaurant.describe_restaurant()
 
 
+#Inheritance
+#The _int_()Method for a Child Class
+class Car():
+    """A simple attepmt to represent a car"""
+
+    def __init__(self, make, model, year):
+        self.model = model
+        self.make = make
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Describe the car"""
+        longname = str(self.year) + ' ' + self.make + ' ' +self.model
+        return longname.title()
+    
+    def read_odometer(self):
+        print("This car has "+ str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+    """Represent aspects of car, specific to electric vehicles."""
+
+    def __init__(self, make , model , year):
+        """Initialize atributes of the parent class."""
+        super().__init__(make, model, year)
+    
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+# The above section tests the working of the child class to make sure it behaves just like the parent class
+
+#Defining Attributes and Methods for the Child Class
+class Car():
+    """A simple attepmt to represent a car"""
+
+    def __init__(self, make, model, year):
+        self.model = model
+        self.make = make
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Describe the car"""
+        longname = str(self.year) + ' ' + self.make + ' ' +self.model
+        return longname.title()
+    
+    def read_odometer(self):
+        print("This car has "+ str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+    """Represent aspects of car, specific to electric vehicles."""
+
+    def __init__(self, make , model , year):
+        """Initialize atributes of the parent class.
+          After initialize attributes Specific to electric vehicles"""
+
+        super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+my_tesla = ElectricCar('telsa', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
+
+#Instances as Attributes
+class Battery():
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size = 70):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+class ElectricCar(Car):
+    """Represent aspects of car, specific to electric vehicles."""
+
+    def __init__(self, make , model , year):
+        """Initialize atributes of the parent class.
+          After initialize attributes Specific to electric vehicles"""
+
+        super().__init__(make, model, year)
+        self.battery_size = Battery()
+
+my_tesla = ElectricCar('tesla', 'model s', 2020)
+print(my_tesla.get_descriptive_name())
+#my_tesla.battery.describe_battery()
 
 
     
